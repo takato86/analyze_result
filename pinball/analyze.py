399fbb760e2bca721f367dfc39404bbfc695a2d3
takato86 <okudo@nii.ac.jp>
2019-11-10 23:02:07 +0900

    [feat] output se.

--- pinball/analyze.py
+++ pinball/analyze.py
@@ -30,13 +30,27 @@ def average_values(file_pattern):
     return mean_total_rewards
 
 
+def output_standard_error(file_pattern):
+    file_list = get_file_list(file_pattern)
+    total_rewards = []
+    for file_path in file_list:
+        total_reward_df = pd.read_csv(file_path, index_col=0)
+        total_reward = total_reward_df.values.reshape(len(total_reward_df.index))
+        total_rewards.append(total_reward.tolist())
+    total_rewards = np.array(total_rewards)
+    std_total_rewards = np.std(total_rewards, axis=0)
+    ste_total_rewards = std_total_rewards / np.sqrt(total_rewards.shape[0])
+    return ste_total_rewards
+
+
 def main():
     argvs = sys.argv[1:]
     processed_v = {}
     for argv in argvs:
-        processed_v[argv] = average_values(argv)
+        processed_v[argv+"_"+"mean"] = average_values(argv)
+        processed_v[argv+"_"+"ste"] = output_standard_error(argv)
     out_dir = 'out'
-    file_name = 'mean_total_reward.csv'
+    file_name = 'mean_ste_total_reward.csv'
     file_path = os.path.join(out_dir, file_name)
     export(file_path, processed_v)
 
